d0b1093d9e46a5c4deb45333bf6d596067b4513c
hmzugur <craxx67@gmail.com>
2021-03-05 16:52:21 +0300

    jumbotron değişim, create.html düzenleme..

--- todo_project_container/todo_app/models.py
+++ todo_project_container/todo_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Todos(models.Model):
+      title = models.CharField(max_length =100)
+      description = models.TextField(blank = True, max_length=2500)
+      finished = models.BooleanField(default = False)
+      date = models.DateTimeField(default = datetime.now ,blank = True)
+
+
+      def __str__(self):
+            return self.title
--- todo_project_container/todo_app/views.py
+++ todo_project_container/todo_app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Todos
 
 def index(request):
+      todo_list = Todos.objects.all()
+    
       
-      return render(request,"todo_pages/index.html") 
+      return render(request,"todo_pages/index.html",{'todo_list':todo_list}) 
 
 
 def about(request):
