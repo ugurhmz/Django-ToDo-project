fa8fe046ae6ffa03dc29967e1ebd0529b570aa8e
hmzugur <craxx67@gmail.com>
2021-03-05 21:04:26 +0300

    update ve son rütuşlar..

--- todo_project_container/todo_app/urls.py
+++ todo_project_container/todo_app/urls.py
@@ -9,5 +9,9 @@ urlpatterns= [
       path('about/',views.about, name='about'),
       path('create/',views.create, name="create"),
       path("delete/<Todos_id>", views.delete, name='delete'),
+      path('yes_finish/<Todos_id>' , views.yes_finish, name="yes_finish"),
+      path('no_finish/<Todos_id>' , views.no_finish, name="no_finish"),
+      path('update/<Todos_id>', views.update, name="update"),
+
 
 ]
--- todo_project_container/todo_app/views.py
+++ todo_project_container/todo_app/views.py
@@ -43,3 +43,30 @@ def delete(request,Todos_id):
       todo = Todos.objects.get(pk=Todos_id)
       todo.delete()
       return redirect("index") #silme olunca yönlendir-> redirect ile, index page
+
+def no_finish(request,Todos_id):
+      todo = Todos.objects.get(pk = Todos_id)
+      todo.finished = True
+      todo.save()
+      return redirect("index")
+
+
+def yes_finish(request,Todos_id):
+      todo = Todos.objects.get(pk = Todos_id)
+      todo.finished =  False
+      todo.save()
+      return redirect("index")
+
+def update(request,Todos_id):
+      if request.method =="POST":
+            todo_list = Todos.objects.get(pk=Todos_id)
+            form = ListForm(request.POST or None, instance=todo_list)
+
+            if form.is_valid: #form validse kaydet
+                  form.save()
+                  return redirect("index")
+
+
+      else:
+            todo_list = Todos.objects.all()
+            return render(request,"todo_pages/update.html",{'todo_list':todo_list}) 
